1575a64955cd2002948efe57ee805853e8e4d0d8
theLucasWalters <lwalters224@gmail.com>
2021-08-30 17:45:03 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import math
+
+root = Tk()
+root.title('InvestiCalc')
+root.iconbitmap('images/investicalc.ico') # <- icon downloaded from https://icon-icons.com/icon/stocks-graphic-with-a-magnifier-tool/70602
+root.geometry('400x200')
+
+def percentChange(get1, get2):
+    # percent change calculator
+    # formula credit: https://www.calculatorsoup.com/calculators/algebra/percent-difference-calculator.php
+
+    global v1, v2
+    v1 = float(get1.get())
+    v2 = float(get2.get())
+
+    ans = ((v2 - v1) / abs(v1)) * 100
+    rndANS = round(ans, 2)
+
+    test = Label(pc, text=f'Answer = {rndANS}%').pack()
+
+def percentChange_window():
+    # percent change calc window
+    global pc
+    pc = Tk()
+    pc.title('Percent Change Calculator')
+    pc.iconbitmap('images/investicalc.ico')
+    pc.geometry('400x200')
+
+    wrn = Label(pc, text="Make sure to remove all commas before pressing 'submit'").pack()
+    num1 = Entry(pc, width=20, borderwidth=2.5)
+    num2 = Entry(pc, width=20, borderwidth=2.5)
+    num1.pack(); num2.pack()
+    calculate = Button(pc, text='Submit', command=lambda: percentChange(get1=num1, get2=num2))
+    calculate.pack()
+
+var = StringVar()
+var.set('Select a calculator:')
+
+calcs = [
+    'Percent Change',
+]
+
+arrow = PhotoImage(file='images/arrow.png') # <- arrow img credit: https://www.flaticon.com/authors/google
+options = OptionMenu(root, var, 'Select a calculator:', *calcs)
+enter = Button(root, text='Go', command=percentChange_window)
+
+options.pack()
+enter.pack()
+
+root.mainloop()
